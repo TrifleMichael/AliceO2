21cf456c0741bbf3cfbf7fd58e270880cadd1665
MichalT <trzebuniak.michal@gmail.com>
2022-07-22 10:34:51 +0200

    Addition test case

--- CCDB/test/testCCDBResponse.cxx
+++ CCDB/test/testCCDBResponse.cxx
@@ -24,10 +24,11 @@ BOOST_AUTO_TEST_CASE(TestCCDBResponseParse)
 {
   std::string* responseString = new std::string(secondFullResponse);
   CCDBResponse ccdbResponse(*responseString);
-  BOOST_CHECK(ccdbResponse.objectNum == 3);
+  BOOST_CHECK(ccdbResponse.objectNum == 4);
   BOOST_CHECK("407f3a65-4c7b-11ec-8cf8-200114580202" == ccdbResponse.getStringAttribute(0, "id"));
   BOOST_CHECK("e5183d1a-4c7a-11ec-9d71-7f000001aa8b" == ccdbResponse.getStringAttribute(1, "id"));
   BOOST_CHECK("52d3f61a-4c6b-11ec-a98e-7f000001aa8b" == ccdbResponse.getStringAttribute(2, "id"));
+  BOOST_CHECK("99d3f61a-4c6b-11ec-a98e-7f000001aa8b" == ccdbResponse.getStringAttribute(3, "id"));
 }
 
 BOOST_AUTO_TEST_CASE(TestCCDBResponseBrowse)
@@ -38,10 +39,11 @@ BOOST_AUTO_TEST_CASE(TestCCDBResponseBrowse)
   CCDBResponse secondCcdbResponse(*secondResponseString);
 
   firstCcdbResponse.browseAndMerge(&secondCcdbResponse);
-  BOOST_CHECK(firstCcdbResponse.objectNum == 3);
+  BOOST_CHECK(firstCcdbResponse.objectNum == 4);
   BOOST_CHECK("407f3a65-4c7b-11ec-8cf8-200114580202" == firstCcdbResponse.getStringAttribute(0, "id"));
   BOOST_CHECK("52d3f61a-4c6b-11ec-a98e-7f000001aa8b" == firstCcdbResponse.getStringAttribute(1, "id"));
   BOOST_CHECK("e5183d1a-4c7a-11ec-9d71-7f000001aa8b" == firstCcdbResponse.getStringAttribute(2, "id"));
+  BOOST_CHECK("99d3f61a-4c6b-11ec-a98e-7f000001aa8b" == firstCcdbResponse.getStringAttribute(3, "id"));
 }
 
 BOOST_AUTO_TEST_CASE(TestCCDBResponseLatest)
@@ -52,7 +54,8 @@ BOOST_AUTO_TEST_CASE(TestCCDBResponseLatest)
   CCDBResponse secondCcdbResponse(*secondResponseString);
 
   firstCcdbResponse.latestAndMerge(&secondCcdbResponse);
-  BOOST_CHECK(firstCcdbResponse.objectNum == 2);
+  BOOST_CHECK(firstCcdbResponse.objectNum == 3);
   BOOST_CHECK("407f3a65-4c7b-11ec-8cf8-200114580202" == firstCcdbResponse.getStringAttribute(0, "id"));
   BOOST_CHECK("52d3f61a-4c6b-11ec-a98e-7f000001aa8b" == firstCcdbResponse.getStringAttribute(1, "id"));
+  BOOST_CHECK("99d3f61a-4c6b-11ec-a98e-7f000001aa8b" == firstCcdbResponse.getStringAttribute(2, "id"));
 }
--- CCDB/test/testCCDBResponseResources.h
+++ CCDB/test/testCCDBResponseResources.h
@@ -88,9 +88,6 @@ const char* firstFullResponse = R"({
                 "/download/52d3f61a-4c6b-11ec-a98e-7f000001aa8b"
             ]
         }
-    ],
-    "subfolders": [
-      "Users/g/grigoras/testing/grid"
     ]
 }
 )";
@@ -200,17 +197,48 @@ const char* secondFullResponse = R"({
             "replicas": [
                 "/download/52d3f61a-4c6b-11ec-a98e-7f000001aa8b"
             ]
+        },
+        {
+            "path": "Users/g/grigoras/testing/grid/c",
+            "createTime": 1637678437647,
+            "lastModified": 1637678437647,
+            "Created": 1637678437647,
+            "Last-Modified": 1637678437647,
+            "id": "99d3f61a-4c6b-11ec-a98e-7f000001aa8b",
+            "validFrom": 1604175160884,
+            "validUntil": 1919535160884,
+            "initialValidity": 1919535160884,
+            "MD5": "c1ffddcd4db30e3e684180165acd63ca",
+            "fileName": "TObject_1595938437506.root",
+            "contentType": "application/octet-stream",
+            "size": 4096,
+            "ETag": "\"52d3f61a-4c6b-11ec-a98e-7f000001aa8b\"",
+            "Valid-From": 1604175160884,
+            "Valid-Until": 1919535160884,
+            "InitialValidityLimit": 1919535160884,
+            "Content-MD5": "c1ffddcd4db30e3e684180165acd63ca",
+            "Content-Disposition": "inline;filename=\"TObject_1595938437506.root\"",
+            "Content-Type": "application/octet-stream",
+            "Content-Length": 4096,
+            "UploadedFrom": "127.0.0.1",
+            "preservation": "false",
+            "ObjectType": "TH1F",
+            "qc_detector_name": "HMP",
+            "custom": "34",
+            "UpdatedFrom": "127.0.0.1",
+            "qc_task_name": "daqTask",
+            "qc_version": "1.1.0",
+            "partName": "send",
+            "replicas": [
+                "/download/52d3f61a-4c6b-11ec-a98e-7f000001aa8b"
+            ]
         }
-    ],
-    "subfolders": [
-      "Users/g/grigoras/testing/grid"
     ]
 }
 )";
 
 const char* emptyResponse = R"({
     "objects": [],
-    "subfolders": []
 })";
 
 } // namespace ccdb
